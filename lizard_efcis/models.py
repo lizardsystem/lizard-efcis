c40207f194aae7c45253cefd0a8caf5af3918d33
AlexandrSeleznev <a.seleznev@hotmail.nl>
2015-01-14 05:03:56 -0800

    AddParameter, WNS models.

--- lizard_efcis/models.py
+++ lizard_efcis/models.py
@@ -9,24 +9,71 @@ from django.utils.translation import ugettext_lazy as _
 from django_hstore import hstore
 
 
-class Status(models.Model):
+ST0 = ""
+ST1 = "Change of interpretation do not use"
+ST2 = "Dubious taxon concept"
+ST3 = "Gepubliceerd"
+ST4 = "Non-taxonomic species group"
+ST5 = "Preferred name"
+ST6 = "Synonym"
+ST7 = "TWN error do not use"
 
-    name = models.CharField(max_length=100)
-
-    def __unicode__(self):
-        return unicode(self.name)
-
-    class Meta:
-        ordering = ['name']
+STATUS_CHOICES = (
+    (ST0, ST0),
+    (ST1, ST1),
+    (ST2, ST2),
+    (ST3, ST3),
+    (ST4, ST4),
+    (ST5, ST5),
+    (ST6, ST6),
+    (ST7, ST7)
+)
 
 
 class Parameter(models.Model):
+    
+    par_code = models.CharField(max_length=30)
+    par_oms = models.CharField(
+        max_length=255, 
+        null=True, 
+        blank=True)
+    casnummer = models.CharField(
+        max_length=30, null=True, blank=True)
+    datum_status = models.DateField(null=True, blank=True)
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        null=True,
+        blank=True)
+    
+    def __unicode__(self):
+        return unicode(self.par_code)
 
-    code = models.CharField(max_length=30)
-    description = models.CharField(max_length=255, null=True, blank=True)
-    cas_number = models.CarField(max_length=30, null=True, blank=True)
-    date_status = models.DateField(null=True, blank=True)
-    status = models.ForeignKey(Status, null=True, blank=True)
+    class Meta:
+        ordering = ['par_code']
+
+
+class WNS(models.Model):
     
-    
+    wns_code = models.CharField(max_length=30)
+    wns_oms = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True)
+    parameter = models.ForeignKey(Parameter, null=True, blank=True)
+    eenheid = models.CharField(max_length=20, null=True, blank=True)
+    hoedanigheid = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True)
+    compartiment = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True)
+    datum_status = models.DateField(null=True, blank=True)
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        null=True,
+        blank=True)
     
