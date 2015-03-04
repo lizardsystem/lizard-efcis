2ffd7d49b62bfe628cd84c4d8d1f1d9a8cc9376c
Reinout van Rees <reinout@vanrees.org>
2015-03-04 11:05:30 +0100

    Commented-out possibly dangerous settings-using filesystemstorage

--- lizard_efcis/models.py
+++ lizard_efcis/models.py
@@ -6,10 +6,11 @@ import datetime
 
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.gis.db import models
-from django.core.files.storage import FileSystemStorage
-from django.conf import settings
+# from django.core.files.storage import FileSystemStorage
+# from django.conf import settings
 
-fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+# fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+# ^^^ TODO: root-level import. Settings might not be fully read. Doesn't seem to be used.
 
 
 class Status(models.Model):
