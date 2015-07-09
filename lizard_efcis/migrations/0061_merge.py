4d8b2264248d038c4fe94e5ec7497654612d13dc
AlexandrSeleznev <a.seleznev@hotmail.nl>
2015-07-09 08:25:52 -0700

    Merging migrations.

---
+++ lizard_efcis/migrations/0061_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lizard_efcis', '0060_locatie_is_krw_area'),
+        ('lizard_efcis', '0058_auto_20150709_1659'),
+    ]
+
+    operations = [
+    ]
