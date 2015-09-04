0f0ed0c0730425fc86cad12154be38c04bedd177
AlexandrSeleznev <a.seleznev@hotmail.nl>
2015-09-04 00:30:49 -0700

    Fix import tasks.

--- lizard_efcis/tasks.py
+++ lizard_efcis/tasks.py
@@ -4,10 +4,10 @@ import logging
 from celery import shared_task
 
 from lizard_efcis.import_data import DataImport
-from lizard_efcis.models import ImportRun
 from lizard_efcis.models import FTPLocation
 
 logger = logging.getLogger(__name__)
+
 
 @shared_task
 def add(x, y):
@@ -22,73 +22,53 @@ def ftpimport():
 
 
 @shared_task
-def check_file(username, importrun=None, *args, **options):
-    """Check passed import_run or
-    all active automatic import_runs"""
-    logger.debug("Begin of check_file task, import_run: '%s'")
-    import_runs = []
+def check_file(username, importrun, *args, **options):
+    """Check passed importrun or
+    all active automatic importruns"""
     data_import = DataImport()
     datetime_format = '%Y-%m-%d %H:%M'
-    if importrun:
-        import_runs.append(importrun)
-        logger.debug("ImportRun: %s" % importrun.name)
+    if importrun is None:
+        logger.debug("Error: ImportRun is None")
+        return
+    importrun.add_log_separator()
+    importrun.add_log_line("Start controle", username=username)
+    importrun.save(force_update=True, update_fields=['action_log'])
+    if importrun.has_xml_attachment:
+        result = data_import.check_xml(importrun, datetime_format)
+    elif importrun.has_csv_attachment:
+        result = data_import.check_csv(importrun, datetime_format)
     else:
-        import_runs = list(ImportRun.objects.filter(
-            **{'type_run': ImportRun.AUTO, 'actief': True}))
-    for import_run in import_runs:
-        import_run.add_log_separator()
-        import_run.add_log_line("Start controle",
-                                username=username)
-        import_run.save(force_update=True, update_fields=['action_log'])
-        if import_run.has_xml_attachment:
-            result = data_import.check_xml(
-                import_run, datetime_format)
-        elif import_run.has_csv_attachment:
-            result = data_import.check_csv(
-                import_run, datetime_format)
-        else:
-            import_run.add_log_line(
-                "Niet uitgevoerd, bestandsextentie is geen .csv of .xml")
-            import_run.save()
-            continue
-        import_run.add_log_line("Controle status is %s" % result)
-        import_run.add_log_line("Eind controle", username=username)
-        import_run.validated = result
-        import_run.uploaded_by = username
-        import_run.save()
+        importrun.add_log_line(
+            "Niet uitgevoerd, bestandsextentie is geen .csv of .xml")
+    importrun.add_log_line("Controle status is %s" % result)
+    importrun.add_log_line("Eind controle", username=username)
+    importrun.validated = result
+    importrun.uploaded_by = username
+    importrun.save()
 
 
 @shared_task
-def import_data(username, importrun=None, *args, **options):
+def import_data(username, importrun, *args, **options):
     """Import data from csv"""
-    import_runs = []
     data_import = DataImport()
     datetime_format = '%Y-%m-%d %H:%M'
     data_import.log = False
-    if importrun:
-        import_runs.append(importrun)
-        logger.debug("ImportRun: %s" % importrun.name)
+    if importrun is None:
+        logger.debug("ImportRun is Nones")
+        return
+
+    importrun.add_log_separator()
+    importrun.add_log_line("Start import", username)
+    importrun.save(force_update=True, update_fields=['action_log'])
+    if importrun.has_xml_attachment:
+        result = data_import.manual_import_xml(importrun, datetime_format)
+    elif importrun.has_csv_attachment:
+        result = data_import.manual_import_csv(importrun, datetime_format)
     else:
-        import_runs = list(ImportRun.objects.filter(
-            **{'type_run': ImportRun.AUTO, 'actief': True}))
-    for import_run in import_runs:
-        import_run.add_log_separator()
-        import_run.add_log_line("Start import",
-                                username)
-        import_run.save(force_update=True, update_fields=['action_log'])
-        if import_run.has_xml_attachment:
-            result = data_import.manual_import_xml(
-                import_run, datetime_format)
-        elif import_run.has_csv_attachment:
-            result = data_import.manual_import_csv(
-                import_run, datetime_format)
-        else:
-            import_run.add_log_line(
-                "Niet uitgevoerd, bestandsextentie is geen .csv of .xml")
-            import_run.save()
-            continue
-        import_run.add_log_line("Import status is %s" % result)
-        import_run.add_log_line("Eind import", username)
-        import_run.imported = result
-        import_run.uploaded_by = username
-        import_run.save()
+        importrun.add_log_line(
+            "Niet uitgevoerd, bestandsextentie is geen .csv of .xml")
+    importrun.add_log_line("Import status is %s" % result)
+    importrun.add_log_line("Eind import", username)
+    importrun.imported = result
+    importrun.uploaded_by = username
+    importrun.save()
