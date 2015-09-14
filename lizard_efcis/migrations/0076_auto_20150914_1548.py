8153a4eb6aa6092b545bfa35697222a2928cd107
Reinout van Rees <reinout@vanrees.org>
2015-09-14 15:57:58 +0200

    Filling krw score 'waarde' for all existing opnames

---
+++ lizard_efcis/migrations/0076_auto_20150914_1548.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+KRW_SCORE_MAPPING = {
+    # Copied to ensure that future migrations keep working
+    'voldoet': 0.9,
+    'voldoet niet': 0.1,
+    'zeer goed': 0.9,
+    'goed': 0.7,
+    'matig': 0.5,
+    'ontoereikend': 0.3,
+    'slecht': 0.1,
+}
+
+
+def fill_krw_scores(apps, schema_editor):
+    Opname = apps.get_model("lizard_efcis", "Opname")
+    for key, value in KRW_SCORE_MAPPING.items():
+        for opname in Opname.objects.filter(waarde_a__iexact=key):
+            print("Re-saving opname %s (waarde_a=%s)" % (opname, waarde_a))
+            opname.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lizard_efcis', '0075_opname_waarde_krw'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill_krw_scores),
+    ]
