6e044d2609f3aeb6eeac715ae733af2754d4f1be
Reinout van Rees <reinout@vanrees.org>
2015-09-30 14:01:47 +0200

    Dialing down from error to warning

--- lizard_efcis/views.py
+++ lizard_efcis/views.py
@@ -688,8 +688,8 @@ class LineAPI(FilteredOpnamesAPIView):
 
         points = list(points)
         if not points:
-            logger.error("Weird. No opnames found in LineAPI for key %s.",
-                         key)
+            logger.warn("Weird. No opnames found in LineAPI for key %s.",
+                        key)
             return Response({})
         first = points[0]
         data = [{'datetime': '%sT%s.000Z' % (point['datum'],
