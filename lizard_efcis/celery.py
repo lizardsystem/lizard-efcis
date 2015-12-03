7f8e41fffea47f9602d23f9940c71f086fec0031
AlexandrSeleznev <a.seleznev@hotmail.nl>
2015-12-03 06:53:29 -0800

    Remove rabbitmq settings.

--- lizard_efcis/celery.py
+++ lizard_efcis/celery.py
@@ -12,7 +12,6 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(
     CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
-    #BROKER_URL='amqp://rabbitmq:7QqBANc@119-rmq-d1.external-nens.local:5672/efcis',
     CELERYBEAT_SCHEDULER='djcelery.schedulers.DatabaseScheduler',
     CELERY_IMPORTS=("lizard_efcis.tasks", ),
 )
