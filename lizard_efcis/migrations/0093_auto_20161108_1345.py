df5390911ce3192926e0174d8573113f50552d09
AlexandrSeleznev <a.seleznev@hotmail.nl>
2016-11-18 01:13:34 -0800

    Edited tooltip.

--- lizard_efcis/migrations/0093_auto_20161108_1345.py
+++ lizard_efcis/migrations/0093_auto_20161108_1345.py
@@ -8,21 +8,21 @@ def set_meetstatus_omschrijving(apps, schema_editor):
     MeetStatus = apps.get_model('lizard_efcis', 'MeetStatus')
     for row in MeetStatus.objects.all():
         if row.naam == "FC-A BIO-A":
-            row.omschrijving = "Fysisch-chemisch actief Biologisch actief"
+            row.omschrijving = "Fysisch-chemisch actief & Biologisch actief"
         elif row.naam == "FC-A BIO-BG":
-            row.omschrijving = "Fysisch-chemisch actief Biologisch buiten gebruik"
+            row.omschrijving = "Fysisch-chemisch actief & Biologisch buiten gebruik"
         elif row.naam == "FC-A BIO-NB":
-            row.omschrijving = "Fysisch-chemisch actief Biologisch niet bemeten"
+            row.omschrijving = "Fysisch-chemisch actief & Biologisch niet bemeten"
         elif row.naam == "FC-BG BIO-A":
-            row.omschrijving = "Fysisch-chemisch buiten gebruik Biologisch actief"
+            row.omschrijving = "Fysisch-chemisch buiten gebruik & Biologisch actief"
         elif row.naam == "FC-NB BIO-A":
-            row.omschrijving = "Fysisch-chemisch niet bemeten Biologisch actief"
+            row.omschrijving = "Fysisch-chemisch niet bemeten & Biologisch actief"
         elif row.naam == "FC-BG BIO-BG":
-            row.omschrijving = "Fysisch-chemisch buiten gebruik Biologisch buiten gebruik"
+            row.omschrijving = "Fysisch-chemisch buiten gebruik & Biologisch buiten gebruik"
         elif row.naam == "FC-NB BIO-BG":
-            row.omschrijving = "Fysisch-chemisch niet bemeten Biologisch buiten gebruik"
+            row.omschrijving = "Fysisch-chemisch niet bemeten & Biologisch buiten gebruik"
         elif row.naam == "FC-BG BIO-NB":
-            row.omschrijving = "Fysisch-chemisch buiten gebruik Biologisch niet bemeten"
+            row.omschrijving = "Fysisch-chemisch buiten gebruik & Biologisch niet bemeten"
         else:
             continue
         row.save()
